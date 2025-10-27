3fa74b24894bfb8bdbe4ee6958b807badde03ed6
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2025-10-26 20:06:00 -0700

    feat: enhance user routes and authentication with token verification and password hashing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+
 import express, { json } from 'express';
 import { connectDB, closeDB } from "./persistence/db/connection.js";
 import notFoundHandler from './middlewares/notFoundHandler.js';
@@ -24,8 +33,10 @@ app.use('/api/v1/protected', protectedRoutes)
 // Rutas de los demas CRUDs
 // Inyección de dependencias mejor practica
 import buildUsersRouter from './routes/users.routes.js';
-app.use('/api/v1/users',verifyToken, buildUsersRouter({ userRepo }));
+app.use('/api/v1/users', buildUsersRouter({ userRepo, verifyToken }));
 
+// A partir de aquí, todas requieren token
+app.use(verifyToken)
 
 // Manejadores de errores
 app.use(notFoundHandler);
